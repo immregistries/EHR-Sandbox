4b16fbcffcc86ecd4b2f499983cd8b1963f27c1a
Clement Hennequin <clement.hennequin@telecomnancy.net>
2022-12-02 09:47:59 -0500

    PractitionerProvider and HAPI FHIR version switched to 6.1.2

---
+++ EHR-Sandbox-Api/src/main/java/org/immregistries/ehr/fhir/PractitionerProvider.java
@@ -0,0 +1,43 @@
+package org.immregistries.ehr.fhir;
+
+import ca.uhn.fhir.rest.annotation.Create;
+import ca.uhn.fhir.rest.annotation.ResourceParam;
+import ca.uhn.fhir.rest.api.MethodOutcome;
+import ca.uhn.fhir.rest.api.server.RequestDetails;
+import ca.uhn.fhir.rest.server.IResourceProvider;
+import org.hl7.fhir.r5.model.Practitioner;
+import org.immregistries.ehr.api.entities.Clinician;
+import org.immregistries.ehr.api.entities.Facility;
+import org.immregistries.ehr.api.repositories.FacilityRepository;
+import org.immregistries.ehr.logic.mapping.PractitionnerHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.server.ResponseStatusException;
+
+@Controller
+public class PractitionerProvider implements IResourceProvider {
+    @Autowired
+    private PractitionnerHandler practitionnerHandler;
+    @Autowired
+    private FacilityRepository facilityRepository;
+
+    @Override
+    public Class<Practitioner> getResourceType() {
+        return Practitioner.class;
+    }
+
+    @Create
+    public MethodOutcome createPractitioner(@ResourceParam Practitioner practitioner, RequestDetails requestDetails) {
+        return createPractitioner(practitioner,
+                facilityRepository.findById(Integer.parseInt(requestDetails.getTenantId()))
+                        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_ACCEPTABLE, "Invalid facility id")));
+    }
+
+    public MethodOutcome createPractitioner(Practitioner practitioner, Facility facility) {
+        MethodOutcome methodOutcome = new MethodOutcome();
+        Clinician clinician = practitionnerHandler.fromFhir(practitioner);
+        // TODO set received information status and make sure history of patient info if already exists
+        return methodOutcome;
+    }
+}
