82e4c6a2d9c47bc18a99766cdaeb47dcb1f63a78
Clement Hennequin <clement.hennequin@nist.gov>
2024-10-02 12:24:24 -0400

    fix on id type

--- EHR-Sandbox-Api/src/main/java/org/immregistries/ehr/api/entities/embedabbles/PatientExternalIdentifierKey.java
+++ EHR-Sandbox-Api/src/main/java/org/immregistries/ehr/api/entities/embedabbles/PatientExternalIdentifierKey.java
@@ -4,5 +4,5 @@ import java.io.Serializable;
 
 public class PatientExternalIdentifierKey implements Serializable {
     private Integer patientId;
-    private String immunizationRegistryId;
+    private Integer immunizationRegistryId;
 }
