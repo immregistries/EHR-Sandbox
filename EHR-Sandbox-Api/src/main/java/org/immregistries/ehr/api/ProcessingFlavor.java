0c85613a45aa388a68aa106393b20f9161ec095b
Clement Hennequin <clement.hennequin@nist.gov>
2025-04-04 11:16:43 -0400

    Progress css for tenant form

--- EHR-Sandbox-Api/src/main/java/org/immregistries/ehr/api/ProcessingFlavor.java
+++ EHR-Sandbox-Api/src/main/java/org/immregistries/ehr/api/ProcessingFlavor.java
@@ -15,7 +15,7 @@ public enum ProcessingFlavor {
     PAIN_PERDU("PAINPERDU", "Includes fields marked as deprecated from codesets in forms and tables"),
     LOTTERY("LOTTERY", "Use external API to verify Lot Number validity in forms"),
     //    LIGUAL("LIGUAL", "Sets all name types as legal"),
-    BIZNESS("BIZNESS", "(Incoming) Uses local id in FHIR references instead of business identifier"),
+//    BIZNESS("BIZNESS", "(Incoming) Uses local id in FHIR references instead of business identifier"),
     R5("R5", "Missing non-critical required fields", true),
     R4("R4", "(In progress, might break some functionalities) Use Fhir R4 version"),
     Z44("Z44", "Use Z44 profile for QBP instead of default Z34"),
