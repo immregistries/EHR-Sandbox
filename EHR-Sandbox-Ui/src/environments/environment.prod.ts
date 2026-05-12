b0b0e306318e0ac04efba24f78fbf2de206f0d76
Clement Hennequin <clement.hennequin@nist.gov>
2026-05-12 10:50:39 -0400

    update project files ui

--- EHR-Sandbox-Ui/src/environments/environment.prod.ts
+++ EHR-Sandbox-Ui/src/environments/environment.prod.ts
@@ -2,6 +2,4 @@ export const environment = {
   production: true,
   //API used in production mode
   apiUrl: "/ehr"
-  // apiUrl: "https://bulksandbox.pagekite.me/ehr-sandbox"
-  // apiUrl: "http://localhost:9091/ehr"
 };
